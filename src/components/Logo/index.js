7c81466223e5e5684ef20438558dbff559a6728f
unknown <nikolovaradmila@gmail.com>
2020-04-30 23:14:58 +0200

    added logo componenent

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,6 +1,21 @@
 import React from "react"
+import styled from "styled-components"
+import { graphql, useStaticQuery } from "gatsby"
+
+/* const getIntroContent = graphql`
+  {
+    contentfulHomePage {
+      homeIntroHeading
+      buttonText
+      homeIntroContent {
+        json
+      }
+    }
+  }
+` */
 
 function Logo() {
+  /*   const data = useStaticQuery(getIntroContent) */
   return <div></div>
 }
 
