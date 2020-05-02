feada39f8b552659b2099597e2e7c08500038a96
unknown <nikolovaradmila@gmail.com>
2020-05-02 23:18:50 +0200

    style changes

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 import styled from "styled-components"
-import { graphql, useStaticQuery } from "gatsby"
 
 const ServiceCard = styled.div`
   height: 30rem;
-  width: 30rem;
+  width: 27rem;
+  margin: 1rem;
   border: 2px solid orange;
 `
 
