cbacee91f10b751ea6dea98d6dbe2e5d5ba44cd2
unknown <nikolovaradmila@gmail.com>
2020-05-11 14:00:58 +0200

    added events

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,6 +1,26 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+const getEvents = graphql`
+  {
+    allContentfulEvent {
+      edges {
+        node {
+          id
+          slug
+          title
+          textContent {
+            json
+          }
+        }
+      }
+    }
+  }
+`
 
 function Events() {
+  const data = useStaticQuery(getEvents)
+
   return <div></div>
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,14 @@ import React from "react"
 import Hero from "../components/Hero"
 import Intro from "../components/Intro"
 import Services from "../components/Services"
+import Events from "../components/Events"
+
 const IndexPage = () => (
   <>
     <Hero></Hero>
     <Intro></Intro>
     <Services></Services>
+    <Events />
   </>
 )
 
