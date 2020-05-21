c251f4ed41dc39f9454cf7d81519bc5db526df7d
unknown <nikolovaradmila@gmail.com>
2020-05-21 19:24:16 +0200

    added content for contact page

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -1,15 +1,29 @@
-import React from "react"
+import React, { useMemo } from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { documentToPlainTextString } from "@contentful/rich-text-plain-text-renderer"
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`
 
 function Event({ event }) {
   const { id, slug, title, textContent, date } = event
+
+  const excerpt = useMemo(
+    () =>
+      documentToPlainTextString(textContent.json)
+        .slice(0, 180)
+        .replace(/\s\w+$/, ""),
+    [textContent.json]
+  )
   return (
-    <Link to={slug} key={id}>
+    <StyledLink to={slug} key={id}>
       <p>{date}</p>
       <h3>{title}</h3>
-      <div>{documentToReactComponents(textContent.json)}</div>
-    </Link>
+      <p> {excerpt} &hellip;</p>
+    </StyledLink>
   )
 }
 
